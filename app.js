6449d072f70b2f1bcfb13e7024af15c08816b5f6
Andrea <andrea.mauri87@gmail.com>
2017-03-13 14:01:53 +0100

    added read and wrtie endpoints

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const proxy = require('./lib/proxy.js')
+
+let app = express()
+
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+
+  proxy.getStatistics((error,result)=>{
+    res.json(error || JSON.parse(result))
+  })
+
+})
+
+app.post('/', function (req, res) {
+  
+  proxy.sendData(req.body,(err,result)=>{
+    res.send(err || result)
+  })
+
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
